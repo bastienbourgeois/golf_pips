322fd99f2638f24ae4d7837abe471d83f33f70c5
Bastien_PC_HP <banebps@hotmail.fr>
2020-08-17 23:47:07 +0200

    Big prog and uk channel update

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ from colorama import init
 init()
 
 CHANNEL_URL = 'http://channelstream.club/'
-NB_CHANNEL = 9
+NB_MORE_CHANNEL = 8
 
 r = requests.get(url=CHANNEL_URL)
-soup = BeautifulSoup(r.text, 'lxml').find_all('div')
+soup = BeautifulSoup(r.content, 'lxml').find_all('div')
 
 class col:
     HEADER = '\033[95m'
@@ -21,29 +21,62 @@ class col:
     FAIL = '\033[91m'
     ENDC = '\033[0m'
 
+dic = {
+    "Golf+": "https://channelstream.club/stream/golf.php",
+    "Foot+": "https://channelstream.club/stream/foot_plus.php",
+    "RMC 1": "https://channelstream.club/stream/rmc_sport-1.php",
+    "Canal+": "https://channelstream.club/stream/canal.php",
+    "Canal+ Sport": "https://channelstream.club/stream/canal_sport.php",
+    "beIN 1": "https://channelstream.club/stream/bein_1.php"
+}
+
+def text_prog(h):
+    i = ""
+    if h.find('i') != None:
+        i = h.find('i').text
+        h.find('i').decompose()
+    else:
+        i = col.HEADER + "présent dans les favoris" + col.ENDC
+    s = h.text + " -> " + i
+    return s
+
+def print_prog():
+    s = col.WARNING + "\t-sans programme-" + col.ENDC
+    for j in range(len(soup)):
+        if soup[j].find_all('input')[1].get('value') == d:
+            s = text_prog(soup[j].find('h4'))
+            return col.OKGREEN + "\t" + s + col.ENDC
+    return s
+
 print(col.WARNING + "------ TV Pip's ------" + col.ENDC)
 print(col.HEADER + "*** Favoris ***" + col.ENDC)
-print(col.OKGREEN + "1- Golf plus")
-print("2- Foot plus")
-print("3- RMC 1")
-print("4- Canal plus")
-print("5- Canal sport")
-print("6- beIN 1" + col.ENDC)
+
+for i, d in enumerate(dic.values()):
+    print(str(i + 1) + "- " + list(dic.keys())[i], end='')
+    print(print_prog())
+    i = i + 1
+
 print(col.HEADER + "\n*** En ce moment ***" + col.ENDC)
 
-channel = []
-channel.append("https://channelstream.club/stream/golf.php")
-channel.append("https://channelstream.club/stream/foot_plus.php")
-channel.append("https://channelstream.club/stream/rmc_sport-1.php")
-channel.append("https://channelstream.club/stream/canal.php")
-channel.append("https://channelstream.club/stream/canal_sport.php")
-channel.append("https://channelstream.club/stream/bein_1.php")
+for i in range(NB_MORE_CHANNEL):
+    url = soup[i].find_all('input')[1].get('value')
+    dic.update( {url.rsplit('/', 1)[-1]: url} )
+    print(str(len(dic)) + "-" + col.OKGREEN + text_prog(soup[i].find('h4')) + col.ENDC)
 
-for i in range(NB_CHANNEL):
-    print(col.OKGREEN + str(i + len(channel) + 1) + "-" + soup[i].find('h4').get_text() + col.ENDC)
+## ALLEMAGNE
+print(col.HEADER + "\n*** Chaines Etrangères ***" + col.ENDC)
+NB_UK_CHANNEL = 4
 
-for i in range(NB_CHANNEL):
-    channel.append(soup[i].find_all('input')[1].get('value'))
+dic.update( {"Sky Sport 1": "https://channelstream.club/stream/uk_skysport_1.php"} )
+dic.update( {"Sky Sport Foot": "https://channelstream.club/stream/uk_skysport_football.php"} )
+dic.update( {"Sky Sport Golf": "https://sportscart.xyz/ch/scplayer-70.php"} )
+dic.update( {"Sky Sport Arena":"https://channelstream.club/stream/uk_skysport_arena.php"} )
+dic.update( {"BT Sport 1": "https://channelstream.club/stream/uk_btsport_1.php"} )
+
+for i in range(len(dic) - NB_UK_CHANNEL, len(dic)):
+    print(str(i + 1) + "- " + list(dic.keys())[i], end='')
+    print(print_prog())
+
 
 print(col.OKBLUE + "\nRentre un nombre, puis fais entrer: ", end='' + col.ENDC)
 
@@ -54,17 +87,14 @@ while True:
         x = int(s)
     if not s.isnumeric():
         print(col.FAIL + "Fait un effort, ce n'est même pas un nombre ça : " + s + ", " + col.OKBLUE + " test 1 idiiiiot: ", end='' + col.ENDC)
-    elif not (x > 0 and x < len(channel) + 1):
+    elif not (x > 0 and x < len(dic) + 1):
         print(col.FAIL + "Ce nombre n'est pas dans la liste," + col.OKBLUE + " test un autre: ", end='' + col.ENDC)
     else:
         break
 
 ## STREAM 
 
-URL = channel[x-1]
-
-CHROME_DRIVER_PATH = 'driver/chromedriver.exe'
-EXTENSION_PATH = 'driver/ub.crx'
+URL = list(dic.values())[x-1]
 
 def del_id(idd):
     js_string = "var element = document.getElementById('" + idd + "');if(element!=null){element.remove();}"
@@ -95,7 +125,7 @@ options.add_argument("--log-level=3")
 options.add_experimental_option("useAutomationExtension", False)
 options.add_experimental_option("excludeSwitches",["enable-automation"])
 
-driver = webdriver.Chrome(options = options, executable_path = CHROME_DRIVER_PATH)
+driver = webdriver.Chrome(options = options, executable_path = 'driver/chromedriver.exe')
 driver.get(URL)
 main_window = driver.current_window_handle
 
@@ -118,13 +148,14 @@ del_all_tag('iframe')
 del_all_tag('object')
 del_id('wrapfabtest')
 
-sleep(1.5)
+sleep(0.5)
 
 for i in range(3):
     webdriver.ActionChains(driver).click().perform()
-    if len(driver.window_handles) > 1:
-        driver.switch_to.window(driver.window_handles[1])
-        driver.close()
-        sleep(0.2)
-        driver.switch_to.window(main_window)
-        sleep(0.5)
+    for j in range(len(driver.window_handles)):
+        if j > 0:
+            driver.switch_to.window(driver.window_handles[j])
+            sleep(0.5)
+            driver.close()
+            sleep(0.5)
+            driver.switch_to.window(main_window)
